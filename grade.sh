cb1a2cd94b9212a9aa9ab8593afe588f5434b0b9
hosaka-elijah <81043167+hosaka-elijah@users.noreply.github.com>
2023-03-01 14:46:28 -0800

    Complete grade.sh

--- grade.sh
+++ grade.sh
@@ -18,28 +18,43 @@ cd student-submission
 cp ../TestListExamples.java ./
 cp -R ../lib ./
 
+grep -q "static List<String> filter(List<String> list, StringChecker sc) {" ListExamples.java
+if [[ $? -ne 0 ]]
+then 
+    echo "Missing filter method signature"
+    exit 1
+fi
+
+grep -q "static List<String> merge(List<String> list1, List<String> list2) {" ListExamples.java
+if [[ $? -ne 0 ]]
+then
+    echo "Missing merge method signature"
+    exit 1
+fi
+
 # Compile
-javac -cp $CPATH *.java 2> log.txt
+javac -cp $CPATH *.java 2> compile.log
 if [[ $? -eq 0 ]]
 then
     echo "Compile successful"
 else
     echo "Could not compile ListExamples.java"
-    echo `cat log.txt`
+    echo `cat compile.txt`
     exit 1
 fi
 
 # Run the tests
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > log.txt
-grep -q "OK" log.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > tester.log
+grep -q "OK" tester.log
 if [[ $? -eq 0 ]]
 then
     echo "Score: 100 / 100"
 else 
     # Parse results for number
-    RESULT=`grep "Tests run:" log.txt`
+    RESULT=`grep "Tests run:" tester.log`
     TESTS=${RESULT:11:1}  # or cut -b 12-
     FAILS=${RESULT:25:1} # or cut -b 26-
     SCORE=$((($TESTS - $FAILS) * 100 / $TESTS))
     echo "Score: $SCORE / 100"
+    echo "test failed: testMergeRightEnd()"
 fi
